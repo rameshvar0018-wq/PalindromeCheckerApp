a802b56a2a80f7a8f80c5a2f90be6b3b988bb8f8
rameshvar0018-wq <rameshavr0018@gmail.com>
2026-02-26 11:14:50 +0530

    UC2 HARDCORD PALINDROME

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,20 @@
 public class PalindromeCheckerApp {
 
-    /**
-     * Application entry point.
-     *
-     * This is the first method executed by the JVM
-     * when the program starts.
-     *
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
