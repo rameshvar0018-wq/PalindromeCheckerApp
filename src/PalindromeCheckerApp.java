3af3370ac777fc3546804407c46f49a6a1eb0f04
rameshvar0018-wq <rameshvar0018@gmail.com>
2026-03-09 21:52:43 +0530

    UC8

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,40 +1,38 @@
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
-            // Define the input string
-            String input = "radar";
+        // Define the input string
+        String input = "level";
 
-            // Create a Deque to store characters
-            Deque<Character> deque = new ArrayDeque<>();
+        // Create a deque to store characters
+        Deque<Character> deque = new ArrayDeque<>();
 
-            // Add each character to the deque
-            for (char c : input.toCharArray()) {
-                deque.addLast(c);
-            }
-
-            // Assume it is palindrome
-            boolean isPalindrome = true;
-
-            // Continue comparison while more than one element exists
-            while (deque.size() > 1) {
-
-                char first = deque.removeFirst();
-                char last = deque.removeLast();
-
-                if (first != last) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-
-            // Print result
-            System.out.println("Palindrome : " + isPalindrome);
+        // Add each character to the deque
+        for (char c : input.toLowerCase().toCharArray()) {
+            deque.addLast(c);
         }
+
+        // Assume it is a palindrome
+        boolean isPalindrome = true;
+
+        // Compare until only one or zero elements remain
+        while (deque.size() > 1) {
+
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
-
-
-
-
+}
